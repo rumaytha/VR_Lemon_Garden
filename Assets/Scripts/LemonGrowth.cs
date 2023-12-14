6e57375f8af8d544df6ef3d21f0008cb469d7211
rumaytha <rumaythalsherbiny@gmail.com>
2023-12-14 16:17:33 +0400

    update LemonGrowth script

--- Assets/Scripts/LemonGrowth.cs
+++ Assets/Scripts/LemonGrowth.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class LemonGrowth : MonoBehaviour
 {
-
     // Flag to track whether the lemon is currently growing
     private bool isGrowing = false;
 
@@ -11,7 +10,7 @@ public class LemonGrowth : MonoBehaviour
     private Vector3 originalScale;
 
     // Cooldown duration after the lemons have shrunk
-    public float cooldownDuration = 5.0f;
+    public float cooldownDuration = 10.0f;
     private bool isCooldown = false;
 
     // Start is called when the script is enabled
@@ -36,26 +35,27 @@ public class LemonGrowth : MonoBehaviour
                 // Start the growth coroutine
                 StartCoroutine(GrowLemon());
             }
+            else
+            {
+                // Lemon is already growing, reset to original size and start cooldown
+                ResetLemon();
+            }
         }
     }
 
-    // Coroutine to gradually grow the lemon to three times its original size
+    // Coroutine to gradually grow the lemon
     private IEnumerator GrowLemon()
     {
         isGrowing = true;
 
-        for (float i = 1.0f; i < 1.3f; i += 0.2f)
+        float elapsedTime = 0f;
+
+        while (elapsedTime <= 100f)
         {
-            float elapsedTime = 0f;
+            transform.localScale = Vector3.Lerp(Vector3.zero, originalScale * 1.3f, elapsedTime);
+            elapsedTime += Time.deltaTime;
 
-            // Gradually increase the scale of the lemon over time
-            while (elapsedTime <= 10f)
-            {
-                transform.localScale = Vector3.Lerp(Vector3.zero, originalScale * i, elapsedTime);
-                elapsedTime += Time.deltaTime;
-
-                yield return null;
-            }
+            yield return null;
         }
 
         // Lemon is no longer growing
@@ -69,5 +69,22 @@ public class LemonGrowth : MonoBehaviour
 
         // Cooldown is over, reset flag
         isCooldown = false;
+    }
+
+    // Function to reset the lemon to its original size
+    private void ResetLemon()
+    {
+        StopAllCoroutines(); // Stop any ongoing growth coroutine
+        transform.localScale = Vector3.zero; // Reset scale to zero
+        isGrowing = false; // Lemon is no longer growing
+        isCooldown = true; // Set cooldown active
+        StartCoroutine(CooldownTimer()); // Start cooldown timer
+    }
+
+    // Coroutine for the cooldown timer
+    private IEnumerator CooldownTimer()
+    {
+        yield return new WaitForSeconds(cooldownDuration);
+        isCooldown = false; // Cooldown is over, reset flag
     }
 }
